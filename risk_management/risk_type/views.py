3e21633b91cec25cb37e97560ee41e4179f49a80
shubhamagarwal003 <shubhamagarwal003@gmail.com>
2018-08-15 23:17:25 +0530

    removing unecessary imports

--- risk_management/risk_type/views.py
+++ risk_management/risk_type/views.py
@@ -1,17 +1,14 @@
 import json
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import RiskField, Risk, RiskFieldEnumOption
-from .enums import RiskTypes, DataTypes
+from .models import Risk
 from .serializer import RiskSerializer
-from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 
 def get_risk(request, risk_id):
     try:
         risk = Risk.objects.filter(id=risk_id).prefetch_related('fields', 'fields__options')
-        if(len(risk) > 0):
+        if(len(risk)):
             response = RiskSerializer(risk[0]).data
             return HttpResponse(json.dumps(response), content_type="application/json")
         else:
